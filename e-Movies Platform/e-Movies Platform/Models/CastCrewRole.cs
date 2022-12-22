1a9dead858a752d12b69794d5c4a770dc1bd11e2
Mentor Jakupi <mj52547@ubt-uni.net>
2022-12-22 23:42:55 +0100

    cast&crew crud

--- e-Movies Platform/e-Movies Platform/Models/CastCrewRole.cs
+++ e-Movies Platform/e-Movies Platform/Models/CastCrewRole.cs
@@ -1,9 +1,11 @@
-﻿namespace e_Movies_Platform.Models
+﻿using Microsoft.Build.Framework;
+
+namespace e_Movies_Platform.Models
 {
     public class CastCrewRole
     {
         public int id { get; set; }
-
-        public string Role { get; set; }
+        
+        public string? Role { get; set; }
     }
 }
